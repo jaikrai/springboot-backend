b1d83614e5b1e035e90a169c9f79eb8d47e1d33a
jaikrai <jaikrai0@gmail.com>
2021-12-21 10:53:20 -0500

    added update and get by id rest call

--- src/main/java/com/springboot/application/controller/EmployeeController.java
+++ src/main/java/com/springboot/application/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.springboot.application.controller;
 
+import com.springboot.application.exception.ResourceNotFountException;
 import com.springboot.application.model.Employee;
 import com.springboot.application.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,7 +18,6 @@ public class EmployeeController {
     private EmployeeRepository repository;
 
     // GET all employee
-
     @GetMapping("/employees")
     public List<Employee> getAllEmployee(){
         return repository.findAll();
@@ -26,6 +27,26 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee){
         return repository.save(employee);
+    }
+
+    // get employee by id rest api
+    @GetMapping("employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = repository.findById(id).orElseThrow(() ->
+            new ResourceNotFountException("Employee not exist with this id" + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    // update employee rest api
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee emplDetail){
+        Employee employee = repository.findById(id).orElseThrow(() ->
+                new ResourceNotFountException("Employee not exist with this id" + id));
+        employee.setFirstName(emplDetail.getFirstName());
+        employee.setLastName(emplDetail.getLastName());
+        employee.setEmail(emplDetail.getEmail());
+       Employee updatedEmployee =  repository.save(employee);
+       return ResponseEntity.ok(updatedEmployee);
     }
 
 }
